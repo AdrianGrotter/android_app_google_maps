7c692b2efd9a9523603d45f34c3cf04bee11b562
JuanMephisto <adrian@grotter.net>
2022-11-06 11:49:12 +0100

    Further work on create attraction page. Submitting now works

--- app/src/main/java/maps/s354378_mappe3/CreateAttractionActivity.java
+++ app/src/main/java/maps/s354378_mappe3/CreateAttractionActivity.java
@@ -3,18 +3,30 @@ package maps.s354378_mappe3;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
 public class CreateAttractionActivity extends AppCompatActivity {
     LatLng myLatLng;
+    EditText description;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_attraction);
         SharedPreferences sp = getSharedPreferences("my_prefs", Activity.MODE_PRIVATE);
@@ -22,8 +34,11 @@ public class CreateAttractionActivity extends AppCompatActivity {
                 Double.longBitsToDouble(sp.getLong("long", 0)) );
         Toast.makeText(this, "Pos: "+myLatLng, Toast.LENGTH_SHORT).show();
 
+        description = (EditText) findViewById(R.id.input_description);
+        name = (EditText) findViewById(R.id.input_name);
+
         Button btnReturn = (Button) findViewById(R.id.btnReturn);
-        Button btnSubmit = (Button)findViewById(R.id.btnReturn);
+        Button btnSubmit = (Button)findViewById(R.id.btnSubmit);
         btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,10 +48,23 @@ public class CreateAttractionActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                activityMain();
+                Attraction a = new Attraction();
+                a.description = description.getText().toString();
+                a.name = name.getText().toString();
+                a.pos = myLatLng;
+                Geocoder coder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> res = coder.getFromLocation(myLatLng.latitude, myLatLng.longitude, 1);
+                    a.address = res.get(0).getAddressLine(0);
+                    Toast.makeText(CreateAttractionActivity.this, "Registered new Attraction!\nName: " + a.getName() + "\nDesc: "+a.getDescription() +
+                            "\nAdress: " + a.getAddress() + "\nLoc: " + a.getPos(), Toast.LENGTH_SHORT).show();
+                    activityMain();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(CreateAttractionActivity.this, "Houston, we have a problem...", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
     }
 
     private void activityMain() {
